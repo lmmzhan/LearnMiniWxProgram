9225530317ebfbd4f40dd9bea68e3b54b3833aa3
zhanjilai <15011580240@163.com>
2019-10-13 22:12:07 +0800

    初体验

--- Pages/home/home.js
+++ Pages/home/home.js
@@ -1,66 +1,47 @@
 // Pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    name: 'xiao北',
+    age: 18,
+    students: [{
+        id: 100,
+        name: '武松',
+        age: 30
+      },
+      {
+        id: 110,
+        name: '林冲',
+        age: 29
+      },
+      {
+        id: 120,
+        name: '鲁智深',
+        age: 32
+      },
+      {
+        id: 130,
+        name: '花荣',
+        age: 18
+      }
+    ],
+    counter: 0
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleBtnClick() {
+    // 1.错误做法，页面不会刷新
+    //  this.data.counter+=1
+    // console.log(this.data.counter)
+    // 2.this.setDat()
+    this.setData({
+      counter: this.data.counter += 1
+    })
   },
+  handleSubtraction() {
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    this.setData({
+      counter: this.data.counter -= 1
+    })
 
   }
+
 })
