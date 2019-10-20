7e950b41d45636515be96206052601fdb3d4c172
zhanjilai <15011580240@163.com>
2019-10-20 14:51:22 +0800

    jiu-01

---
+++ components/my-cpn.js
@@ -0,0 +1,26 @@
+// components/my-cpn.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+    title: "煮酒论英雄",
+    content: '天下英雄唯是君与操耳'
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
