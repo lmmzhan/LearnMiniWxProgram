076eb7cf181c3528a521c067266da77a458a997a
zhanjilai <15011580240@163.com>
2019-10-22 23:25:38 +0800

    shi_04share

--- Pages/showtoast/showtoast.js
+++ Pages/showtoast/showtoast.js
@@ -8,36 +8,36 @@ Page({
 
   },
 
-  handleShowToast(){
+  handleShowToast() {
     wx.showToast({
       title: '我是标题',
-      duration :3000,
-      icon:'loading',
-      mask:true,   //遮挡，自动取消
-      success:function(res){
-        console.log('展示成功',res)
+      duration: 3000,
+      icon: 'loading',
+      mask: true, //遮挡，自动取消
+      success: function(res) {
+        console.log('展示成功', res)
       },
-      fail: function () {
+      fail: function() {
         console.log('展示弹框失败')
       },
-      complete: function () {
+      complete: function() {
         console.log('完成函数的调用')
       },
     })
 
   },
 
-  handleShowModal(){
+  handleShowModal() {
     wx.showModal({
       title: '温馨提示',
       content: '个低嘴不甜，长得难看还没钱',
       cancelText: '退出',
-      success:function(res){
+      success: function(res) {
         console.log(res)
-        if(res.cancel){
+        if (res.cancel) {
           console.log('用户点击了取消')
         }
-        if(res.confirm){
+        if (res.confirm) {
           console.log('用户点击了确定')
         }
       }
@@ -46,32 +46,36 @@ Page({
   },
 
 
-  handleShowLoading(){
+  handleShowLoading() {
     wx.showLoading({
       title: '加载中你',
-      mask:true
+      mask: true
     })
-     
-     //利用定时器，延时消失
-     setTimeout(()=>{
-       wx.hideLoading()
-     },2000)
 
+    //利用定时器，延时消失
+    setTimeout(() => {
+      wx.hideLoading()
+    }, 2000)
   },
 
-  handleShowActionSheet(){
+  handleShowActionSheet() {
     wx.showActionSheet({
-      itemList: ['相册','相机'],
-      itemColor:'red',
-      success:function(res){
-        console.log('点击了',res)
+      itemList: ['相册', '相机'],
+      itemColor: 'red',
+      success: function(res) {
+        console.log('点击了', res)
       }
 
     })
+  },
 
+// 分享的代码
+  onShareAppMessage(options) {
+    return {
+      title: '你好，我来分享',
+      imageUrl: '/Pages/assets/home/add.png'
+      // imageUrl: 'https://gss3.bdstatic.com/-Po3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=1ba7eb8df7f2b211f0238d1cabe90e5d/fd039245d688d43f8c0b1c0c711ed21b0ff43ba9.jpg'
+    }
   }
-
-
-
 
 })
